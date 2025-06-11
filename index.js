0a6ad5ca72c7187f4ea963303f45119bcf0c53e3
Annrich <annrichlawrence002@gmail.com>
2025-06-11 14:47:21 +0530

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/getdata/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.send("trial");
+});
+app.post('/createdata',(req,res)=>{ 
+    const data =req.body;
+    console.log(data);
+    res.send('Data received'); 
+});
+
+
+const port=3001; 
+app.listen(port,()=>{
+console.log("server running on port",{port});
+})
